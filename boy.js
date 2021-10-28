a0c570f8716e453d44772e8205e7848f6d5bae9f
Judech <judefuanyi@gmail.com>
2021-10-28 15:45:56 -0700

    some beauty

--- boy.js
+++ boy.js
@@ -1,3 +1,7 @@
 // right tab
 const currentDate = new Date();
 document.getElementById("date1").innerHTML = currentDate.toDateString();
+
+$(document).bind("contextmenu",function(e){
+    return false;
+      });
